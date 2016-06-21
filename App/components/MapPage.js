7d00559f0d5835f9b6663cf90c865ccafcffb2ec
Adam Lessen <lessena@gmail.com>
2016-06-20 20:12:16 -0700

    Add function to post place to server

--- App/components/MapPage.js
+++ App/components/MapPage.js
@@ -5,6 +5,7 @@ import {
   View,
   Image,
   TouchableHighlight,
+  TextInput,
 } from 'react-native';
 
 import SubmitPage from './SubmitPage';
@@ -14,10 +15,24 @@ class MapPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: '',
-      password: '',
+      username: 'Adam',
+      userId: '10',
+      place: '',
+      note: '',
       showProgess: false,
     };
+  }
+
+  handlePlace(text) {
+    this.setState({
+      place: text,
+    });
+  }
+
+  handleNote(text) {
+    this.setState({
+      note: text,
+    });
   }
 
   hideBar() {
@@ -25,10 +40,34 @@ class MapPage extends Component {
   }
 
   submitLocation() {
-    this.props.navigator.push({
-      component: SubmitPage,
-      title: 'Submit Page',
-      passProps: { hideBar: this.hideBar.bind(this) },
+    const userId = this.state.userId;
+    const note = this.state.note;
+    const place = this.state.place;
+
+    console.log('Clicked!');
+    console.log(`place is: ${this.state.place}
+      note is: ${this.state.note}`);
+    const body = {
+      userId,
+      place,
+      note,
+    };
+    const url = `http://localhost:7000/api/users/${this.state.userId}/places`;
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
+    .then((response) => {
+      console.log(response);
+      if (response.status === 200) {
+        
+      }
+    })
+    .catch((error) => {
+      console.warn(error);
     });
   }
 
@@ -38,6 +77,16 @@ class MapPage extends Component {
         <Image style={styles.logo}
         source={{uri:'http://2.bp.blogspot.com/-IELsSax8WPg/Tyzsu05V8qI/AAAAAAAAAWU/qbPzat5H2Oc/s400/Map_pin2.png'}} />
         <Text> Logged In </Text>
+        <TextInput
+          onChangeText={(text) => { this.handlePlace(text); }}
+          style={styles.input}
+          placeholder='Name of place'
+        />
+        <TextInput
+          onChangeText={(text) => { this.handleNote(text); }}
+          style={styles.input}
+          placeholder='Note about place'
+        />
         <TouchableHighlight style={styles.button}>
           <Text
             style={styles.buttonText}
@@ -71,6 +120,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
 
+  },
+  input: {
+    height: 50,
+    marginTop: 10,
+    padding: 4,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#ff0066',
   },
 
   buttonText: {
